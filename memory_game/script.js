3f7aa4a703a991bbdba7d612ce336a306eea2d71
LucasSaladini <64647470+LucasSaladini@users.noreply.github.com>
2022-06-05 17:09:20 -0300

    Implemented function to reset board

--- memory_game/script.js
+++ memory_game/script.js
@@ -5,7 +5,8 @@ let blockBoard = false
 
 function flipCard() {
     if(blockBoard) return
-    
+    if(this === firstCard) return
+
     this.classList.add('flip')
 
     if(!hasFlippedCard) {
@@ -31,13 +32,24 @@ function checkForMath() {
 function disabledCards() {
     firstCard.removeEventListener('click', flipCard)
     secondCard.removeEventListener('click', flipCard)
+
+    resetBoard()
 }
 
 function unflipCards() {
+    blockBoard = true
+
     setTimeout(() => {
         firstCard.classList.remove('flip')
         secondCard.classList.remove('flip')
+
+        resetBoard()
     }, 1500)
+}
+
+function resetBoard() {
+    [hasFlippedCard, blockBoard] = [false, false]
+    [firstCard, secondCard] = [null, null]
 }
 
 cards.forEach((card) => {
