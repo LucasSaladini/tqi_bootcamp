14789fb3a6e0e5d015503bfc686ec764332aa417
LucasSaladini <64647470+LucasSaladini@users.noreply.github.com>
2022-06-07 11:13:54 -0300

    How render works

---
+++ reactjs/render/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+const element = 'Digital Innovation One';
+const element2 = <h1>Olá, mundo!</h1>
+
+function App() {
+  return (
+    <div className="App">
+      {element}
+      {element2}
+    </div>
+  );
+}
+
+export default App;
