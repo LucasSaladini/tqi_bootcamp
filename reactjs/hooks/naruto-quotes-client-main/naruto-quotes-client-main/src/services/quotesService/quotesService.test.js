21d47c2e57ae87231b472dc046cb27c621b5983d
LucasSaladini <64647470+LucasSaladini@users.noreply.github.com>
2022-06-08 15:53:19 -0300

    Created service to fetch quotes

--- reactjs/hooks/naruto-quotes-client-main/naruto-quotes-client-main/src/services/quotesService/quotesService.test.js
+++ reactjs/hooks/naruto-quotes-client-main/naruto-quotes-client-main/src/services/quotesService/quotesService.test.js
@@ -1,13 +1,12 @@
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
-
 import { getQuote } from './quotesService';
 
 const response = { test: 'testing' };
 
 const server = setupServer(
   rest.get(process.env.REACT_APP_API, (req, res, ctx) => {
-    return res(ctx.json(response));
+    return res(ctx.json(response))
   })
 );
 
@@ -15,8 +14,8 @@ beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
-test('transforms json response into object', async () => {
+test('transform json into object', async () => {
   const quote = await getQuote();
 
   expect(quote).toStrictEqual(response);
-});
+})
