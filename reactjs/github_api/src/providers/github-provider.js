9bc9980b6c184960034d0fabacd7b30baee766bd
LucasSaladini <64647470+LucasSaladini@users.noreply.github.com>
2022-06-18 17:51:35 -0300

    Created provider to consume info from GitHub

---
+++ reactjs/github_api/src/providers/github-provider.js
@@ -0,0 +1,36 @@
+import React, { createContext, useState } from 'react'
+
+export const GithubContext = createContext({
+    user: {},
+    repositories: [],
+    starred: [],
+})
+
+const GithubProvider = ({ children }) => {
+    {
+        const [githubState, setGithubState] = useState({
+           user: {
+            login: undefined,
+            name: undefined,
+            publicUrl: undefined,
+            blog: undefined,
+            company: undefined,
+            location: undefined,
+            followers: 0,
+            following: 0,
+            public_gists: 0,
+            public_repos: 0,
+           }
+        })
+
+        const contextValue = {
+            githubState,
+        }
+
+        return (
+            <GithubProvider.Provider value={contextValue}>{children}</GithubProvider.Provider>
+        )
+    }
+}
+
+export default GithubProvider
