2ff8951a2884ef0a8ed99db0ee246f8c8a1b9b61
LucasSaladini <64647470+LucasSaladini@users.noreply.github.com>
2022-06-18 17:40:31 -0300

    Mocked repository item

--- reactjs/github_api/src/components/repositories/index.js
+++ reactjs/github_api/src/components/repositories/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import RepositoryItem from '../repository-item'
 import * as S from './styled'
 
 const Repositories = () => {
@@ -8,8 +9,18 @@ const Repositories = () => {
                 <S.WrapperTab>Repositoires</S.WrapperTab>
                 <S.WrapperTab>Starred</S.WrapperTab>
             </S.WrapperTabList>
-            <S.WrapperTabPanel>Panel Repositories</S.WrapperTabPanel>
-            <S.WrapperTabPanel>Panel Starred</S.WrapperTabPanel>
+            <S.WrapperTabPanel>
+                <RepositoryItem name='calculator' 
+                    linkToRepo='https://github.com/LucasSaladini/calculator'
+                    fullName='lucassaladini/calculator'
+                    />
+            </S.WrapperTabPanel>
+            <S.WrapperTabPanel>
+            <RepositoryItem name='filmesAPI' 
+                    linkToRepo='https://github.com/LucasSaladini/filmesAPI'
+                    fullName='lucassaladini/filmesAPI'
+                    />
+            </S.WrapperTabPanel>
         </S.WrapperTabs>
     )
 }
