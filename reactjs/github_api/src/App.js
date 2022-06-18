3829741291ff0517cc0322fa072e84aac3dbecd6
LucasSaladini <64647470+LucasSaladini@users.noreply.github.com>
2022-06-18 11:19:44 -0300

    Populated profile component

--- reactjs/github_api/src/App.js
+++ reactjs/github_api/src/App.js
@@ -1,30 +1,12 @@
 import React from "react";
 import Layout from "./components/layout";
+import Profile from "./components/profile";
 
 function App() {
   return (
     <main className="App">
       <Layout>
-        <div>
-          <img src="https://avatars.githubusercontent.com/u/64647470?v=4" alt="Avatar of user" />
-          <h1>Lucas Saladini</h1>
-          <h3>Username: </h3>
-          <span>LucasSaladini</span>
-          <div>
-            <div>
-              <h4>Followers: </h4>
-              <span>5</span>
-            </div>
-            <div>
-              <h4>Starreds: </h4>
-              <span>5</span>
-            </div>
-            <div>
-              <h4>Followings: </h4>
-              <span>5</span>
-            </div>
-          </div>
-        </div>
+        <Profile />
         <div>Repositories</div>
         <div>Starreds</div>
       </Layout>
--- reactjs/github_api/src/components/profile/index.js
+++ reactjs/github_api/src/components/profile/index.js
@@ -3,7 +3,26 @@ import * as S from './styled'
 
 const Profile = () => {
   return (
-    <S.Wrapper></S.Wrapper>
+    <S.Wrapper>
+        <img src="https://avatars.githubusercontent.com/u/64647470?v=4" alt="Avatar of user" />
+          <h1>Lucas Saladini</h1>
+          <h3>Username: </h3>
+          <span>LucasSaladini</span>
+          <div>
+            <div>
+              <h4>Followers: </h4>
+              <span>5</span>
+            </div>
+            <div>
+              <h4>Starreds: </h4>
+              <span>5</span>
+            </div>
+            <div>
+              <h4>Followings: </h4>
+              <span>5</span>
+            </div>
+          </div>
+    </S.Wrapper>
   )
 }
 
