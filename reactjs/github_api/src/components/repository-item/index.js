61bcf4042db786fd202f4cac8b9135ac8f135f25
LucasSaladini <64647470+LucasSaladini@users.noreply.github.com>
2022-06-18 17:37:04 -0300

    Created component for each repository

--- reactjs/github_api/src/components/repository-item/index.js
+++ reactjs/github_api/src/components/repository-item/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const RepositoryItem = () => {
   return (
     <div>
-        <h2>app-ideas</h2>
+        <h2>calculator</h2>
         <h4>full name: </h4>
         <a href='https://github.com/LucasSaladini' target="_blank" rel="noreferrer">lucassaladini/app-ideas</a>
     </div>
