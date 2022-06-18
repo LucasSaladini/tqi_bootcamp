b5f47cf2603ba77c5da6c9afc01a566caa9d79ef
LucasSaladini <64647470+LucasSaladini@users.noreply.github.com>
2022-06-18 11:44:06 -0300

    Styled components inside main page

--- reactjs/github_api/src/components/profile/index.js
+++ reactjs/github_api/src/components/profile/index.js
@@ -4,24 +4,30 @@ import * as S from './styled'
 const Profile = () => {
   return (
     <S.Wrapper>
-        <img src="https://avatars.githubusercontent.com/u/64647470?v=4" alt="Avatar of user" />
-          <h1>Lucas Saladini</h1>
-          <h3>Username: </h3>
-          <span>LucasSaladini</span>
-          <div>
+        <S.Image src="https://avatars.githubusercontent.com/u/64647470?v=4" alt="Avatar of user" />
+        <S.WrapperInfoUser>
             <div>
-              <h4>Followers: </h4>
-              <span>5</span>
+                <h1>Lucas Saladini</h1>
+                <S.WrapperUsername>
+                    <h3>Username: </h3>
+                    <a href='https://github.com/LucasSaladini' target="_blank" rel="noreferrer">LucasSaladini</a>
+                </S.WrapperUsername>
             </div>
-            <div>
-              <h4>Starreds: </h4>
-              <span>5</span>
-            </div>
-            <div>
-              <h4>Followings: </h4>
-              <span>5</span>
-            </div>
-          </div>
+            <S.WrapperStatusCount>
+                <div>
+                <h4>Followers</h4>
+                <span>5</span>
+                </div>
+                <div>
+                <h4>Starreds</h4>
+                <span>5</span>
+                </div>
+                <div>
+                <h4>Followings</h4>
+                <span>5</span>
+                </div>
+            </S.WrapperStatusCount>
+        </S.WrapperInfoUser>
     </S.Wrapper>
   )
 }
