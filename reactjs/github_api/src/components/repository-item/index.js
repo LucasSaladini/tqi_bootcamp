e1f2c07ad7523de383856069982241219a511c59
LucasSaladini <64647470+LucasSaladini@users.noreply.github.com>
2022-06-18 17:42:16 -0300

    Used props to fetch info

--- reactjs/github_api/src/components/repository-item/index.js
+++ reactjs/github_api/src/components/repository-item/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const RepositoryItem = () => {
+const RepositoryItem = ({ name, linkToRepo, fullName}) => {
   return (
     <div>
-        <h2>calculator</h2>
+        <h2>{name}</h2>
         <h4>full name: </h4>
-        <a href='https://github.com/LucasSaladini' target="_blank" rel="noreferrer">lucassaladini/calculator</a>
+        <a href={linkToRepo} target="_blank" rel="noreferrer">{fullName}</a>
     </div>
   )
 }
