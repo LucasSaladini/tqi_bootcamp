536ff01475a07bb5521836ef68a684b674879154
LucasSaladini <64647470+LucasSaladini@users.noreply.github.com>
2022-06-18 11:10:38 -0300

    Mocked up app info

--- reactjs/github_api/src/App.js
+++ reactjs/github_api/src/App.js
@@ -5,7 +5,26 @@ function App() {
   return (
     <main className="App">
       <Layout>
-        <div>User</div>
+        <div>
+          <img src="https://avatars.githubusercontent.com/u/64647470?v=4" alt="Avatar of user" />
+          <h1>Lucas Saladini</h1>
+          <h3>Username: </h3>
+          <span>LucasSaladini</span>
+          <div>
+            <div>
+              <h4>Followers: </h4>
+              <span>5</span>
+            </div>
+            <div>
+              <h4>Starreds: </h4>
+              <span>5</span>
+            </div>
+            <div>
+              <h4>Followings: </h4>
+              <span>5</span>
+            </div>
+          </div>
+        </div>
         <div>Repositories</div>
         <div>Starreds</div>
       </Layout>
