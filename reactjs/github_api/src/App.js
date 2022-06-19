ea7f048249d55ceee06791f40f26bfef55e42338
LucasSaladini <64647470+LucasSaladini@users.noreply.github.com>
2022-06-19 17:07:27 -0300

    Imported component to App

--- reactjs/github_api/src/App.js
+++ reactjs/github_api/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Layout from "./components/layout";
+import NoSearch from "./components/no-search";
 import Profile from "./components/profile";
 import Repositories from "./components/repositories";
 import useGithub from './hooks/github-hooks'
@@ -17,7 +18,7 @@ function App() {
             <Repositories />
           </>}
         </> :
-      <div>Nenhum usuário pesquisado</div>
+      <NoSearch />
      } 
     </Layout>
   );
