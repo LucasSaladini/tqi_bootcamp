822e9138b98910a6712615c1914b7c974ff8e34e
LucasSaladini <64647470+LucasSaladini@users.noreply.github.com>
2022-06-19 10:31:02 -0300

    Implemented search component

--- reactjs/github_api/src/components/layout/index.js
+++ reactjs/github_api/src/components/layout/index.js
@@ -1,13 +1,12 @@
 import React from 'react'
+import Header from '../header'
 import * as S from './styled'
 
 function Layout({ children }) {
   return (
     <S.WrapperLayout>
-        <header>
-            Header
-        </header>
-        {children}
+      <Header />
+      {children}
     </S.WrapperLayout>
   )
 }
