5f08ea49534eb8c89718e1532a197037f1f1ee7f
LucasSaladini <64647470+LucasSaladini@users.noreply.github.com>
2022-07-01 09:12:14 -0300

    Calculator class

---
+++ java/first_project/src/first_project/calculator/Calculator.java
@@ -0,0 +1,44 @@
+package first_project.calculator;
+import java.util.Scanner;
+
+public class Calculator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try (Scanner scan = new Scanner(System.in)) {
+			int a, b;
+			
+			System.out.println("Digite o primeiro valor: ");
+			a = scan.nextInt();
+			System.out.println("Digite o segundo valor: ");
+			b = scan.nextInt();
+			
+			int sum = sum(a,b);
+			int subtraction = subtraction(a,b);
+			int multiplication = multiplication(a,b);
+			float division = division(a,b);
+			
+			System.out.println("soma " + sum);
+			System.out.println("subtação " + subtraction);
+			System.out.println("multiplicação " + multiplication);
+			System.out.println("divisão " + division);
+		}
+	}
+	
+	public static int sum(int a, int b) {
+		return a + b;
+	}
+	public static int subtraction(int a, int b) {
+		return a - b;
+	}
+	public static float division(int a, int b) {
+		return a / b;
+	}
+	public static int multiplication(int a, int b) {
+		return a * b;
+	}
+	
+	
+}
