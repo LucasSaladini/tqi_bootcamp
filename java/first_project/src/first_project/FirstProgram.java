9e1667f805da808cda0bb6e2e3e85c7734f20187
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2022-07-01 08:52:30 -0300

    Initial commit for java

---
+++ java/first_project/src/first_project/FirstProgram.java
@@ -0,0 +1,13 @@
+package first_project;
+
+import first_project.model.Cat;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// System.out.println("Hello World");
+		
+		Cat gato = new Cat();
+	}
+
+}
