fd43b673c6cba98988d686458a03a7ec08978de5
Lucas Saladini <64647470+LucasSaladini@users.noreply.github.com>
2022-07-01 09:13:51 -0300

    Created calculator class

--- java/first_project/src/first_project/FirstProgram.java
+++ java/first_project/src/first_project/FirstProgram.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package first_project;
 
 import first_project.model.Cat;
@@ -11,3 +12,18 @@ public class FirstProgram {
 	}
 
 }
+=======
+package first_project;
+
+import first_project.model.Cat;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// System.out.println("Hello World");
+		
+		Cat gato = new Cat();
+	}
+
+}
+>>>>>>> 9e1667f805da808cda0bb6e2e3e85c7734f20187
--- java/first_project/src/first_project/model/Cat.java
+++ java/first_project/src/first_project/model/Cat.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package first_project.model;
 
 import java.util.Objects;
@@ -69,3 +70,76 @@ public class Cat {
 	 */
 
 }
+=======
+package first_project.model;
+
+import java.util.Objects;
+
+public class Cat {
+
+	private String nome;
+	private String cor;
+	private String idade;
+
+	public Cat() {
+	}
+
+	public Cat(String nome, String cor, String idade) {
+		this.nome = nome;
+		this.cor = cor;
+		this.idade = idade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cor, idade, nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cat other = (Cat) obj;
+		return Objects.equals(cor, other.cor) && Objects.equals(idade, other.idade) && Objects.equals(nome, other.nome);
+	}
+
+	public String getIdade() {
+		return idade;
+	}
+
+	@Override
+	public String toString() {
+		return "Cat [nome=" + nome + ", cor=" + cor + ", idade=" + idade + "]";
+	}
+
+	public void setIdade(String idade) {
+		this.idade = idade;
+	}
+
+	/*
+	 * public Cat(String nome, String cor, String idade) { this.nome = nome;
+	 * this.cor = cor; this.idade = idade; }
+	 */
+
+}
+>>>>>>> 9e1667f805da808cda0bb6e2e3e85c7734f20187
